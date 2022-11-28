05cc4e7ad8bde9802ec5cf8ea450c9002e46331f
Aminul Islam <aminulds61@gmail.com>
2022-11-28 23:15:51 +0600

    header spacing issue sovle

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,18 +12,20 @@ const Header = () => {
   };
 
   return (
-    <header className='flex items-center justify-between max-w-6xl mx-auto px-3 bg-white border-b shadow-sm sticky top-0 z-50'>
-      <div>
-        <img onClick={() => navigate('/')} className='h-14 cursor-pointer' src={logo} alt="logo" />
-      </div>
-      <div>
-        <ul className='flex space-x-8'>
-          <li onClick={() => navigate('/')} className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathRoute('/') && 'text-black border-b-red-500'}`}>Home</li>
-          <li onClick={() => navigate('/offers')} className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathRoute('/offers') && 'text-black border-b-red-500'}`}>Offers</li>
-          <li onClick={() => navigate('/sign-in')} className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathRoute('/sign-in') && 'text-black border-b-red-500'}`}>Signin</li>
-        </ul>
-      </div>
-    </header>
+    <section className=' bg-white border-b shadow-sm sticky top-0 z-50'>
+      <header className='flex items-center justify-between max-w-6xl mx-auto px-3'>
+        <div>
+          <img onClick={() => navigate('/')} className='h-14 cursor-pointer' src={logo} alt="logo" />
+        </div>
+        <div>
+          <ul className='flex space-x-8'>
+            <li onClick={() => navigate('/')} className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathRoute('/') && 'text-black border-b-red-500'}`}>Home</li>
+            <li onClick={() => navigate('/offers')} className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathRoute('/offers') && 'text-black border-b-red-500'}`}>Offers</li>
+            <li onClick={() => navigate('/sign-in')} className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathRoute('/sign-in') && 'text-black border-b-red-500'}`}>Signin</li>
+          </ul>
+        </div>
+      </header>
+    </section>
   )
 }
 
