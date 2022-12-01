aabfb4540a34e344501a3fc395b66450babba72d
Aminul Islam <aminulds61@gmail.com>
2022-12-02 00:13:03 +0600

    update profile done

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,10 +1,14 @@
-import { getAuth } from 'firebase/auth';
+import { getAuth, updateProfile } from 'firebase/auth';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import { async } from '@firebase/util';
+import { doc, updateDoc } from 'firebase/firestore';
+import { db } from '../firebase';
 
 const Profile = () => {
   const navigate = useNavigate();
+  const [changeProfile, setChangeProfile] = useState(false);
   const auth = getAuth();
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
@@ -18,6 +22,33 @@ const Profile = () => {
     toast.success("Sign Out!");
   };
 
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value
+    }))
+  };
+
+  const onSubmit = async () => {
+    try {
+      if (auth.currentUser.displayName !== name) {
+        // update firebase auth
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        });
+
+        // update firestore user db
+        const docRef = doc(db, "users", auth.currentUser.uid)
+        await updateDoc(docRef, {
+          name,
+        });
+
+      }
+    } catch (error) {
+      toast.error("Couldn't Update Profile!");
+    }
+  };
+
   return (
     <>
       <section className='max-w-6xl mx-auto flex justify-center items-center flex-col'>
@@ -25,12 +56,17 @@ const Profile = () => {
 
         <div className='w-full md:w-[50%] mt-6 px-3'>
           <form >
-            <input type="text" id='name' value={name} disabled className='mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out' />
+            <input type="text" id='name' value={name} disabled={!changeProfile} onChange={onChange} className={`mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out ${changeProfile && "bg-red-200 focus:bg-red-200"}`} />
 
-            <input type="email" id='email' value={email} disabled className='mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out' />
+            <input type="email" id='email' value={email} disabled className={`mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out`} />
 
             <div className='flex justify-between whitespace-nowrap text-sm sm:text-lg mb-6'>
-              <p className='text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer font-semibold'>Upate Profile</p>
+              <p onClick={() => {
+                changeProfile && onSubmit();
+                setChangeProfile((prevState) => !prevState)
+              }} className='text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer font-semibold'>{
+                  changeProfile ? 'Apply Update' : 'Upate Profile'
+                }</p>
               <p onClick={onSignOut} className='text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out cursor-pointer font-semibold'>Sign out</p>
             </div>
 
