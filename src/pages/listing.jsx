a1a95b0c53be891bcb5b2119855c350f96f0779a
Aminul Islam <aminulds61@gmail.com>
2022-12-05 22:22:46 +0600

    share link added

--- src/pages/listing.jsx
+++ src/pages/listing.jsx
@@ -7,11 +7,13 @@ import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css'
 import 'swiper/css/bundle';
+import { FaShare } from 'react-icons/fa';
 
 const Listing = () => {
   const params = useParams();
   const [listing, setListing] = useState();
   const [loading, setLoading] = useState(true);
+  const [shreLinkCopy, setShreLinkCopy] = useState(false);
 
   useEffect(() => {
     const fetchListing = async () => {
@@ -32,17 +34,6 @@ const Listing = () => {
 
   return (
     <main>
-
-      {/* <Swiper slidesPerView={1} navigation pagination={{ type: "progressbar" }} effect="fade" modules={[EffectFade]} autoplay={{ delay: 3000 }}>
-        {listing.imgUrls.map((url, index) => (
-          <SwiperSlide key={index}>
-            <div className="w-full overflow-hidden h-[300px]" style={{ background: `url(${listing.imgUrls[index]}) center no-repeat` }}>
-
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper> */}
-
       <Swiper
         // install Swiper modules
         modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
@@ -61,8 +52,22 @@ const Listing = () => {
             </div>
           </SwiperSlide>
         ))}
-
       </Swiper>
+
+      <div onClick={() => {
+        navigator.clipboard.writeText(window.location.href)
+        setShreLinkCopy(true);
+        setTimeout(() => {
+          setShreLinkCopy(false);
+        }, 2000)
+      }} className="fixed top-[12%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center">
+        <FaShare className="text-lg text-slate-500" />
+      </div>
+      {shreLinkCopy && (
+        <p className="fixed top-[23%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2">Link Copied!</p>
+      )}
+
+
     </main>
   )
 }
